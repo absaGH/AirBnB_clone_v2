59af59091eda96f1a96ea3675588df40871eeb39
root <absa3852@gmail.com>
2021-08-12 08:05:18 +0000

    fixes

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,6 @@ from sqlalchemy import String
 Base = declarative_base()
 
 
-
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
@@ -20,29 +19,25 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-       # if not kwargs:
-       #     from models import storage
-       self.id = str(uuid.uuid4())
-       self.created_at = datetime.utcnow()
-       self.updated_at = datetime.utcnow()
+        self.id = str(uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
-        #    storage.new(self)
-        #else:
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != "__class__":
-                    #print("setting attribute")
-                    #print(key, value)
+                    # print("setting attribute")
+                    # print(key, value)
                     setattr(self, key, value)
 
-            #kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            # kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
             #                                         '%Y-%m-%dT%H:%M:%S.%f')
-            #kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            # kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
             #                                         '%Y-%m-%dT%H:%M:%S.%f')
-            #del kwargs['__class__']
-            #self.__dict__.update(kwargs)
+            # del kwargs['__class__']
+            # self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -60,7 +55,7 @@ class BaseModel:
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         dictionary.pop("_sa_instance_state", None)
@@ -69,4 +64,3 @@ class BaseModel:
     def delete(self):
         """Delete the current instance from storage"""
         models.storage.delete(self)
-
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,11 +11,11 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is not None:
-            if type(cls) == str:
+            if isinstance(cls, str):
                 cls = eval(cls)
             cls_dict = {}
             for k, v in self.__objects.items():
-                if type(v) == cls:
+                if isinstance(v, cls):
                     cls_dict[k] = v
             return cls_dict
         return FileStorage.__objects
@@ -24,7 +24,7 @@ class FileStorage:
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
-    Def save(self):
+    def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
@@ -44,16 +44,16 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
