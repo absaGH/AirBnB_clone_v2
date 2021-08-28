b1f34cb510c24159eb2662744ca1101cdd844c85
absaGH <absa3852@gmail.com>
2021-08-28 12:18:51 +0000

    webstaticDeploy

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Sets up a web server for deployment of web_static
+
+apt-get update
+apt-get install -y nginx
+
+
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '29i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
+
